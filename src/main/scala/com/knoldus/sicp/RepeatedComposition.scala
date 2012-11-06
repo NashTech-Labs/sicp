ba8275e2753565a678f43176c9edf1d5f48bf89d
ayushmishra2005 <ayush@knoldus.com>
2012-11-06 23:32:01 +0530

    Changes  in RepeatedComposition

--- src/main/scala/com/knoldus/sicp/RepeatedComposition.scala
+++ src/main/scala/com/knoldus/sicp/RepeatedComposition.scala
@@ -1,4 +1,4 @@
-package com.knoldus.sicp
+package com.knoldus.caketest
 
 object RepeatedComposition extends App {
 
@@ -7,17 +7,17 @@ object RepeatedComposition extends App {
   /**
    * Square of a number
    */
-  def square(x: Int) =  x * x
+  def square(x: Int) = x * x
 
   /**
    * Takes one argument functions f and a positive integer n for nth repeated function
    * Returns itself until n=1
    */
   def compose(f: Int => Int, x: Int, n: Int): Int = {
-    if (n == 1) {
-      f(x)
-    } else {
-      compose(f, f(x), n - 1)
+    n match {
+      case 0 => x
+      case 1 => f(x)
+      case _ => compose(f, f(x), n - 1)
     }
   }
 
@@ -28,7 +28,13 @@ object RepeatedComposition extends App {
   def repeated(f: Int => Int, n: Int) = {
     x: Int => compose(f, f(x), n - 1)
   }
-  
-  println(repeated(square , 2)(5)) //Output is 625
-  
+
+  println(repeated(square, 2)(5)) //Output is 625
+
+  println(repeated(square, 1)(5)) //Output is 25
+
+  println(repeated(square, 2)(2)) //Output is 16
+
+  println(repeated(square, 2)(3)) //Output is 81
+
 }
